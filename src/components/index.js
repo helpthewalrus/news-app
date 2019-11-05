3b3b9fb20e563701292c87402ae699884c5b9536
Alina Tylets <alina_tylets@epam.com>
2019-11-05 12:26:01 +0300

    add componets export index file and import AppComponent from it in main index.js

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { AppComponent } from "./AppComponent/index";
+export { ErrorMessage } from "./ErrorMessage/index";
+export { NewsList } from "./NewsList/index";
+export { NewsSourcesList } from "./NewsSourcesList/index";
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles/index.scss";
 
-import { AppComponent } from "./components/AppComponent/index";
+import { AppComponent } from "./components/index";
 
 const app = new AppComponent();
 app.init();
